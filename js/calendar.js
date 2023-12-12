c0dbcfd77dd449e25e332fe21c28bd9b1dc1e189
yliew2022 <yliew2022@fau.edu>
2023-12-12 14:40:48 -0500

    added event deatil over hover in calendar functionality

--- js/calendar.js
+++ js/calendar.js
@@ -1,62 +1,110 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const prevButton = document.getElementById('prev-month');
-  const nextButton = document.getElementById('next-month');
-  const currentMonth = document.getElementById('current-month');
-  const dates = document.getElementById('dates');
-  const today = new Date();
-  let currentMonthDate = new Date(today.getFullYear(), today.getMonth(), 1);
+document.addEventListener('DOMContentLoaded', function () {
+    const prevButton = document.getElementById('prev-month');
+    const nextButton = document.getElementById('next-month');
+    const currentMonth = document.getElementById('current-month');
+    const dates = document.getElementById('dates');
+    const today = new Date();
+    let currentMonthDate = new Date(today.getFullYear(), today.getMonth(), 1);
 
-  function renderCalendar() {
-      currentMonth.textContent = new Date(currentMonthDate).toLocaleString('default', { month: 'long', year: 'numeric' });
-      dates.innerHTML = '';
+    function renderCalendar() {
+        fetch('http://localhost:5678/events')
+            .then(response => response.json())
+            .then(data => {
+                if (Array.isArray(data.events)) {
+                    const daysInMonth = new Date(currentMonthDate.getFullYear(), currentMonthDate.getMonth() + 1, 0).getDate();
+                    const firstDay = new Date(currentMonthDate.getFullYear(), currentMonthDate.getMonth(), 1).getDay();
 
-      const daysInMonth = new Date(currentMonthDate.getFullYear(), currentMonthDate.getMonth() + 1, 0).getDate();
-      const firstDay = new Date(currentMonthDate.getFullYear(), currentMonthDate.getMonth(), 1).getDay();
+                    currentMonth.textContent = new Date(currentMonthDate).toLocaleString('default', { month: 'long', year: 'numeric' });
+                    dates.innerHTML = '';
 
-      for (let i = 0; i < firstDay; i++) {
-          const emptyDate = document.createElement('div');
-          emptyDate.className = 'date-box';
-          dates.appendChild(emptyDate);
-      }
+                    for (let i = 0; i < firstDay; i++) {
+                        const emptyDate = document.createElement('div');
+                        emptyDate.className = 'date-box';
+                        dates.appendChild(emptyDate);
+                    }
 
-      for (let day = 1; day <= daysInMonth; day++) {
-        const dateItem = document.createElement('div');
-        dateItem.className = 'date-box';
-        dateItem.textContent = day;
-      
-        // Check if there's an event for the current date (you should replace this condition with your own logic)
-        const hasEvent = hasEventForDate(currentMonthDate.getFullYear(), currentMonthDate.getMonth(), day);
-      
-        const eventInfo = document.createElement('div');
-        eventInfo.className = 'event-info';
-      
-        // If there's an event, add a symbol (you can customize the symbol as needed)
-        if (hasEvent) {
-          const eventSymbol = document.createElement('span');
-          eventSymbol.className = 'event-symbol';
-          eventSymbol.textContent = '•'; // You can use any symbol or text here
-          eventInfo.appendChild(eventSymbol);
+                    for (let day = 1; day <= daysInMonth; day++) {
+                        const dateItem = document.createElement('div');
+                        dateItem.className = 'date-box';
+                        dateItem.textContent = day;
+
+                        const eventInfo = document.createElement('div');
+                        eventInfo.className = 'event-info';
+
+                        const hasEvent = data.events.some(event => {
+                            const eventDate = new Date(event.date);
+                            return (
+                                eventDate.getFullYear() === currentMonthDate.getFullYear() &&
+                                eventDate.getMonth() === currentMonthDate.getMonth() &&
+                                eventDate.getDate() === day
+                            );
+                        });
+
+                        if (hasEvent) {
+                            const eventSymbol = document.createElement('span');
+                            eventSymbol.className = 'event-symbol';
+                            eventSymbol.textContent = 'EVENT';
+                            eventInfo.appendChild(eventSymbol);
+                            eventInfo.classList.add('has-event');
+                            eventInfo.addEventListener('mouseover', () => displayEventInfo(eventInfo, data.events));
+                            eventInfo.addEventListener('mouseout', () => document.body.removeChild(document.querySelector('.event-tooltip')));
+                        }
+
+                        dateItem.appendChild(eventInfo);
+                        dates.appendChild(dateItem);
+                    }
+                } else {
+                    console.error('"events" property is not an array in the fetched data.');
+                }
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }
+
+    function displayEventInfo(eventInfo, events) {
+        const day = eventInfo.parentElement.textContent;
+        const event = events.find(event => {
+            const eventDate = new Date(event.date);
+            return (
+                eventDate.getFullYear() === currentMonthDate.getFullYear() &&
+                eventDate.getMonth() === currentMonthDate.getMonth() &&
+                eventDate.getDate() === parseInt(day)
+            );
+        });
+    
+        if (event) {
+            const tooltip = document.createElement('div');
+            tooltip.className = 'event-tooltip';
+
+            const eventName = event.eventName || 'Event Name not available';
+            const eventDetails = event.eventDetails || 'Details not available';
+            const eventImage = event.image || 'Image not available';
+            const eventDateStr = event.date || 'Date not available';
+
+            tooltip.innerHTML = `
+                <div>
+                    <img src="${eventImage}" alt="Event Image" style="max-width: 300px; max-height: 300px;">
+                </div>
+                <div>Event: ${eventName}</div>
+                <div>Details: ${eventDetails}</div>
+                <div>Date: ${eventDateStr}</div>
+            `;
+            document.body.appendChild(tooltip);
+            const rect = eventInfo.getBoundingClientRect();
+            tooltip.style.position = 'absolute';
+            tooltip.style.top = `${rect.top - tooltip.offsetHeight}px`;
+            tooltip.style.left = `${rect.left + (rect.width - tooltip.offsetWidth) / 2}px`;
         }
-      
-        dateItem.appendChild(eventInfo);
-        dates.appendChild(dateItem);
-      }
-      
-      function hasEventForDate(year, month, day) {
-        return false;
-      }
-  }
+    }
 
-  prevButton.addEventListener('click', function() {
-      currentMonthDate.setMonth(currentMonthDate.getMonth() - 1);
-      renderCalendar();
-  });
+    prevButton.addEventListener('click', function () {
+        currentMonthDate.setMonth(currentMonthDate.getMonth() - 1);
+        renderCalendar();
+    });
 
-  nextButton.addEventListener('click', function() {
-      currentMonthDate.setMonth(currentMonthDate.getMonth() + 1);
-      renderCalendar();
-  });
+    nextButton.addEventListener('click', function () {
+        currentMonthDate.setMonth(currentMonthDate.getMonth() + 1);
+        renderCalendar();
+    });
 
-  renderCalendar();
-
+    renderCalendar();
 });
